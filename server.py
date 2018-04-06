55f6e460b91322adb017c5685dba7f9ec8cbf910
october10th <venitdubitare@gmail.com>
2018-04-06 16:01:15 -0400

    can delete friend now, going to fix logout

--- server.py
+++ server.py
@@ -443,6 +443,29 @@ def add_friend_act():
         flash('error')
     # return render_template("show_restaurant_detail.html", messages={"rid":restaurant['rid']})
     return redirect(url_for('show_restaurant_details', rid=info['rid']))
+@app.route('/del_friend_act')
+def del_friend_act():
+    # print(request.form['review_text'])
+    info={}
+    info['rid']=request.args.get('rid')
+    info['uid']=request.args.get('uid')
+
+    username="guest"
+    if session.get('logged_in'):
+        username=session['u_name']
+    else:
+        # you cannot add a friend without login
+        return redirect(url_for('show_restaurant_details', rid=info['rid']))
+    friend={}
+    friend['uid_a']=session['uid']
+    friend['uid_b']=info['uid']
+    try:
+        g.conn.execute('DELETE FROM friends WHERE uid_a=%(uid_a)s AND uid_b=%(uid_b)s', friend)
+        
+    except:
+        flash('error')
+    # return render_template("show_restaurant_detail.html", messages={"rid":restaurant['rid']})
+    return redirect(url_for('show_restaurant_details', rid=info['rid']))
 # some TODOs
 # bookmarks
 # write tip
